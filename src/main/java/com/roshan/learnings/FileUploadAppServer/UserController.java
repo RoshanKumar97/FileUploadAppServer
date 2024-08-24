95cd385708cbbb6d8bfd4a63c9a74b3bfe08c2b1
roshan <Roshan97@>
2024-08-24 14:14:01 +0530

    Implementing pagination

--- src/main/java/com/roshan/learnings/FileUploadAppServer/UserController.java
+++ src/main/java/com/roshan/learnings/FileUploadAppServer/UserController.java
@@ -1,6 +1,8 @@
 package com.roshan.learnings.FileUploadAppServer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,27 +27,35 @@ public class UserController {
         List<User> users = new ArrayList<>();
         List<UserDto> userDtos = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
-            String line = reader.readLine();
-            String[] userData = line.split(",");
-            User user = new User();
-            user.setUsername(userData[0]);
-            user.setEmail(userData[1]);
-            user.setPassword(userData[2]);
-            user.setMobile(userData[3]);
-            users.add(user);
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] userData = line.split(",");
+                User user = new User();
+                user.setUsername(userData[0]);
+                user.setEmail(userData[1]);
+                user.setPassword(userData[2]);
+                user.setMobile(userData[3]);
+                users.add(user);
+            }
+            users.forEach((user) -> {
+                if (!userService.userExistsInDb(user)) {
+                    userService.saveUser(user);
+                    UserDto userDto = Adaptor.convertToDto(user);
+                    userDtos.add(userDto);
+                }
+                else
+                    System.out.println("User already exits with the same email!");
+            });
         }
         catch (Exception e) {
             e.printStackTrace();
         }
-        users.forEach((user) -> {
-            if (!userService.userExistsInDb(user)) {
-                userService.saveUser(user);
-                UserDto userDto = Adaptor.convertToDto(user);
-                userDtos.add(userDto);
-            }
-            else
-                throw new RuntimeException("User already exits with the same email!");
-        });
+
         return new ResponseEntity<>(userDtos, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public Page<User> getUsers(@RequestParam int page, @RequestParam int size) {
+        return userService.getUsers(PageRequest.of(page, size));
     }
 }
